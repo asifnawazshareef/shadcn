7f48abdda2ebfb4b62b3b53bd4ea0aca1752318d
asifnawaz <asifnawazshareef@gmail.com>
2024-09-05 20:01:40 +0500

    shadcn updated

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+const ChaiPage = () => {
+  return (
+    <main className="flex justify-center items-center h-[100vh]">
+      <Button variant={"outline"}>Click me</Button>
+    </main>
+  );
+};
+
+export default ChaiPage;
